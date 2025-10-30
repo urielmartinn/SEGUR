004f863f5ca9eea346b77fcc3ea0a93aba0b59b8
urielmartinn <70901942+urielmartinn@users.noreply.github.com>
2025-10-30 17:31:31 +0100

    Update item listing page titles and links

--- app/src/items.php
+++ app/src/items.php
@@ -1,21 +1,21 @@
 <?php
-// Listado: localhost:81/items
+
 require_once __DIR__.'/db.php';
 $result = $mysqli->query("SELECT id,title,artist FROM items");
 ?>
 <!DOCTYPE html>
 <html>
-<head><meta charset="utf-8"><title>Items</title></head>
+<head><meta charset="utf-8"><title>Diskak</title></head>
 <body>
-  <h2>Listado de Items</h2>
-  <a href="/add_item">Añadir item</a>
+  <h2>Disken lista</h2>
+  <a href="/add_item">Diska berria ipini</a>
   <ul>
   <?php while($row = $result->fetch_assoc()): ?>
     <li>
       <?=htmlspecialchars($row['title'])?> - <?=htmlspecialchars($row['artist'])?>
-      [<a href="/show_item?item=<?=urlencode($row['id'])?>">Ver</a>]
-      [<a href="/modify_item?item=<?=urlencode($row['id'])?>">Modificar</a>]
-      [<a href="/delete_item?item=<?=urlencode($row['id'])?>">Eliminar</a>]
+      [<a href="/show_item?item=<?=urlencode($row['id'])?>">Ikusi</a>]
+      [<a href="/modify_item?item=<?=urlencode($row['id'])?>">Aldatu</a>]
+      [<a href="/delete_item?item=<?=urlencode($row['id'])?>">Kendu</a>]
     </li>
   <?php endwhile; ?>
   </ul>
